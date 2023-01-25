78e47c80b83b3bfb3a2d256bf0656695b6fc21e3
Antonio <antoniovictorbordignon@outlook.com>
2023-01-25 15:25:26 -0300

    created-connection-with-api

--- src/components/NewQuote.tsx
+++ src/components/NewQuote.tsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const url = axios.create({
+    baseURL: "https://type.fit/api/quotes"
+})
+
 export function NewQuote(){
+    const [getQuote, setGetQuote] = useState([]) 
+
+    const getPosts = async() => {
+        
+        try {
+            const response = await axios.get("https://type.fit/api/quotes");
+            const numHandom = () => Math.floor(Math.random() * response.data.length) + 1;
+            
+            const quote = response.data[numHandom()]
+
+            console.log(quote)
+           
+            setGetQuote(quote)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getPosts()
+    },[])
+
     return (
-        <h2 className="text-white">test</h2>
-    )
+        <div className="text-white">
+            <h1>
+                {getQuote.length === 0 ? (<p>Loading...</p>) : (
+                        <div id="quote">
+                            <h2>
+                                {getQuote.text}
+                            </h2>
+                            <span>
+                                {getQuote.author}
+                            </span>
+                        </div>
+                )}
+            </h1>
+        </div>
+    );
 }
